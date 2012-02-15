572263f50f5293da8472bc02917c5eae0302b5bf
Andreas Köberle <koeberle@gmail.com>
2012-02-15 22:43:04 +0100

    start test for mvp

---
+++ mvpTest/mvpTest.dart
@@ -0,0 +1,23 @@
+#import('dart:html');
+#import('../testing/unittest/unittest_dartest.dart');
+#import('../testing/dartest/dartest.dart');
+#import('../mvp/mvp.dart');
+
+class mvpTest {
+
+  mvpTest() {
+  }
+
+  void run() {
+    group('Collection', function(){
+      test('', function(){
+        
+      });
+    });
+  }
+}
+
+void main() {
+  new mvpTest().run();
+  new DARTest().run();
+}
